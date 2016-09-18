b4e94143773aa03d518d3654a77e3dac6a2b5afc
Przemyslaw Lenik <przemyslaw.lenik@capgemini.com>
2016-09-18 18:14:45 +0200

    cleaned out unnecessary files

--- ChessClient/app/main/main.module.js
+++ ChessClient/app/main/main.module.js
@@ -2,7 +2,7 @@ angular.module('app.main', ['ngRoute', 'app.main.templates', 'ui.bootstrap'])
     .config(function ($routeProvider) {
         'use strict';
         $routeProvider
-            .when('/', {redirectTo: '/main/welcome'})
-            .when('/main/welcome', {templateUrl: 'main/welcome/welcome.html'})
+            .when('/', {redirectTo: '/component-1/dialog-a'})
+            .when('/component-1/dialog-a', {templateUrl: 'component-1/dialog-a/dialog-a.tpl.html'})
             .otherwise({templateUrl: 'main/page-not-found/page-not-found.html'});
     });
